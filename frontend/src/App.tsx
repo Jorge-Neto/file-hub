f6b9639f60a019929aabcb8596335289331569c1
Jorge-Neto <neto_tunas@hotmail.com>
2023-12-07 00:20:16 -0300

    feat(Main) Added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div``;
---
+++ frontend/src/Components/Header.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+function Header() {
+
+    return (
+        <Container>
+          <Wrapper>
+            <Logo>
+              <img src="" alt="" />
+              <span>Drive</span>
+            </Logo>
+          </Wrapper>
+        </Container>
+      )
+}
+
+export default Header;
+
+const Container = styled.div``;
+const Wrapper = styled.div``;
+const Logo = styled.div``;
